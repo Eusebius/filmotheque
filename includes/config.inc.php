02eef57e6dca9f733b12e4863ef93d88df03d007
Eusebius <eusebius@eusebius.fr>
2016-03-04 17:01:56 +0100

    Preparing 0.2.8

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -55,7 +55,7 @@ $_SESSION['config']['db_prefix'] = '';
  * Current software version
  * *DON'T TOUCH THIS*
  */
-$_SESSION['config']['version'] = "0.2.7";
+$_SESSION['config']['version'] = "0.2.8";
 
 /**
  *  Assigned background colours for each medium quality
