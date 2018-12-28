4f74f43f25924141171f25ecec429472474199f6
Eusebius <eusebius@eusebius.fr>
2018-12-28 17:59:07 +0100

    Updating copyright years

--- scripts/disconnect.php
+++ scripts/disconnect.php
@@ -10,7 +10,7 @@
  */
 /*
   Filmothèque
-  Copyright (C) 2012-2016 Eusebius (eusebius@eusebius.fr)
+  Copyright (C) 2012-2018 Eusebius (eusebius@eusebius.fr)
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
